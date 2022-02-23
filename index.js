b6b691e45e51fa6ea840b43a2ff457ab790bdbe2
stephenlambertfs <97977357+stephenlambertfs@users.noreply.github.com>
2022-02-23 16:31:30 -0700

    commit tree 1f77eaaad77ba6257976ad8ac0db3bf004279e3b

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 16;
+let num4 = 6;
+
+let multiply = num1 * num2;
+
+let random = Math.round(Math.random() * 2 ) + 1;
+
+let mod = num3%num4;
+
+let max = Math.max(10,15,20);
+
+
